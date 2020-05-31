977c94f8b419a046396cdad5a58383091e4eec2d
Rasha Ali <rashaali84@gmail.com>
2020-05-31 10:42:34 +0200

    Update index.js

--- logic/values-for-key/index.js
+++ logic/values-for-key/index.js
@@ -16,32 +16,3 @@ const valuesForKey = (pokeArray, key) => {
 
 module.exports = valuesForKey;
 
-/* https://lodash.com/docs/4.17.15#isEqual
-
-  isEqual is a function to compare objects and arrays.
-
-  it will come in handy on this challenge
-
-*/
-
-/*  arrayWithKey = pokeArray.map(item => { return item[key] });
-  //arrayWithKey.splice(0, arrayWithKey.length, ...(new Set(arrayWithKey)));
-  const setArray = new Set(arrayWithKey.map(x => JSON.stringify(x)))
-  let uniqArray = [];
-  if (!['prev_evolution', 'next_evolution'].includes(key))
-    uniqArray = [...setArray].map(x => JSON.parse(x))
-
-
-  let expected = [];
-  expected = _.without(uniqArray, undefined);
-
-  if (['prev_evolution', 'next_evolution'].includes(key)) {
-    let diff_expected = [...setArray];
-    diff_expected = _.without(diff_expected, undefined);
-    let uniqArray = diff_expected.map(x => JSON.parse(x))
-
-    return uniqArray;
-  }
-  else {
-    return expected;
-  }*/
